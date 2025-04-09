b7609ffe34c97682d50a73acd9fa5797b68efd30
Vinicius Altrao <viniciusaltrao@gmail.com>
2025-04-09 21:38:35 +0200

    feat: add logout route

---
+++ app/api/logout/route.ts
@@ -0,0 +1,21 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request: NextRequest) {
+    const authHeader = request.headers.get('Authorization')
+
+    if (!authHeader) {
+        return NextResponse.json({ error: 'Unauthorized' }, { status: 401 })
+    }
+
+    try {
+        const res = await fetch('http://localhost:8080/api/logout', {
+            headers: {
+                'Authorization': authHeader
+            }
+        })
+
+        return NextResponse.json(await res.json(), { status: res.status })
+    } catch (error) {
+        return NextResponse.json({ error: 'Failed to connect to backend' }, { status: 401 })
+    }
+}
